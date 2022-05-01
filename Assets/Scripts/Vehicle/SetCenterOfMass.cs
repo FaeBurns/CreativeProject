e992ae3cd7d1d3b4394800988dd0be7430e5a82b
bean <titless.skittle@gmail.com>
2022-05-01 15:49:55 +0100

    Added Terrain, Adjusted Center Of Mass on Vehicle

---
+++ Assets/Scripts/Vehicle/SetCenterOfMass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+/// <summary>
+/// Component responsible for setting the center of mass on a parent rigidbody.
+/// </summary>
+public class SetCenterOfMass : MonoBehaviour
+{
+    [SerializeField] private Rigidbody parentRigidbody;
+
+    private void Awake()
+    {
+        parentRigidbody.centerOfMass = transform.localPosition;
+    }
+}
