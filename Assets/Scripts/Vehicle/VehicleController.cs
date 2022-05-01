6c88654a950cf1fd5abcedc44688c21df74c63ec
bean <titless.skittle@gmail.com>
2022-05-01 13:56:52 +0100

    Removed Redundant code from VehicleController

--- Assets/Scripts/Vehicle/VehicleController.cs
+++ Assets/Scripts/Vehicle/VehicleController.cs
@@ -23,8 +23,6 @@ public class VehicleController : MonoBehaviour
     [SerializeField] private float exitThreshold = 1.3f;
     [SerializeField] private Transform exitLocation;
 
-    private delegate void WheelColliderDelegate(WheelCollider collider);
-
     /// <summary>
     /// Freezes all motion.
     /// </summary>
@@ -32,10 +30,10 @@ public class VehicleController : MonoBehaviour
     public void ToggleFreeze(bool enable)
     {
         float brakeForce = enable ? 0 : breakingForce;
-        RunOnWheelCollection(allWheels, c => c.brakeTorque = brakeForce);
+        allWheels.Execute(c => c.brakeTorque = brakeForce);
 
         // always set acceleration to 0
-        RunOnWheelCollection(allWheels, c => c.motorTorque = 0);
+        allWheels.Execute(c => c.motorTorque = 0);
 
         rb.isKinematic = !enable;
     }
@@ -62,30 +60,17 @@ public class VehicleController : MonoBehaviour
         float currentBrakeForce = Input.GetKey(KeyCode.Space) ? breakingForce : 0;
 
         // apply acceleration to wheels
-        RunOnWheelCollection(allWheels, c => c.motorTorque = currentAcceleration);
+        allWheels.Execute(c => c.motorTorque = currentAcceleration);
 
         // apply brake to wheels
-        RunOnWheelCollection(allWheels, c => c.brakeTorque = currentBrakeForce);
+        allWheels.Execute(c => c.brakeTorque = currentBrakeForce);
 
         // steer on input
         float currentSteeringAngle = maxSteerAngle * Input.GetAxis("Horizontal");
 
         // turn specific wheels
         // invert steering of back wheels
-        RunOnWheelCollection(frontSteeringWheels, c => c.steerAngle = currentSteeringAngle);
-        RunOnWheelCollection(backSteeringWheels, c => c.steerAngle = -currentSteeringAngle);
-    }
-
-    /// <summary>
-    /// Executes a delegate on a <see cref="WheelCollider"/> collection.
-    /// </summary>
-    /// <param name="collection">The collection to operate on.</param>
-    /// <param name="wheelDelegate">The delegate to run on each element in the collection.</param>
-    private void RunOnWheelCollection(IEnumerable<WheelCollider> collection, WheelColliderDelegate wheelDelegate)
-    {
-        foreach (WheelCollider collider in collection)
-        {
-            wheelDelegate.Invoke(collider);
-        }
+        frontSteeringWheels.Execute(c => c.steerAngle = currentSteeringAngle);
+        backSteeringWheels.Execute(c => c.steerAngle = -currentSteeringAngle);
     }
 }
